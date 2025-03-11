c9ccc9695eaf8c0ae626371bdfa79f5b87fab767
Oliver Seidl <seidl.oliver@icloud.com>
2025-03-11 16:49:10 +0100

    Update Analytics import in layout.tsx from /react to /next, which should ensure proper analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from '@/components/theme-provider'
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
